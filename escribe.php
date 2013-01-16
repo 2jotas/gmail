7892ace82ef08dae2fef0f8fb3ad1a2065c294d4
2jotas <2jota27@gmail.com>
2013-01-16 02:05:55 -0500

    Try 1 correos

--- escribe.php
+++ escribe.php
@@ -1,14 +1,21 @@
 <?php
 require_once('class.phpmailer.php'); //Incluimos la clase phpmailer
 
+//$fp = fopen('datos.txt','a') or die("Problemas al crear");
+
+//foreach($_GET as $clave => $valor)
+//{
+//fwrite($fp,"\n".$clave." => ".$valor."\n");
+//}
+//fclose($fp);
+
 $mail = new PHPMailer(true); // Declaramos un nuevo correo, el parametro true significa que mostrara excepciones y errores.
 
 $mail->IsSMTP(); // Se especifica a la clase que se utilizará SMTP
 
 //------------------------------------------------------
   $correo_emisor="jortizpinturaspinturil@gmail.com";     //Correo a utilizar para autenticarse
-					     //Gmail o de GoogleApps
-  $nombre_emisor="Heroku";               //Nombre de quien envía el correo
+  $nombre_emisor="pinturil";               //Nombre de quien envía el correo
   $contrasena="hasacell";          //contraseña de tu cuenta en Gmail
   $correo_destino="2jota27@gmail.com";      //Correo de quien recibe
   $nombre_destino="2Jotas";                //Nombre de quien recibe   	
