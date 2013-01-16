a0211067dc2007c715f3d5c697cf29978477d1ea
2jotas <2jota27@gmail.com>
2013-01-16 02:39:24 -0500

    Desactivado Captcha Gmail para envio masivo correos

--- escribe.php
+++ escribe.php
@@ -1,22 +1,14 @@
 <?php
 require_once('class.phpmailer.php'); //Incluimos la clase phpmailer
 
-//$fp = fopen('datos.txt','a') or die("Problemas al crear");
-
-//foreach($_GET as $clave => $valor)
-//{
-//fwrite($fp,"\n".$clave." => ".$valor."\n");
-//}
-//fclose($fp);
-
 $mail = new PHPMailer(true); // Declaramos un nuevo correo, el parametro true significa que mostrara excepciones y errores.
 
 $mail->IsSMTP(); // Se especifica a la clase que se utilizará SMTP
 
 //------------------------------------------------------
-  $correo_emisor="2jota27@gmail.com";     //Correo a utilizar para autenticarse
-  $nombre_emisor="sonny";               //Nombre de quien envía el correo
-  $contrasena="hasacell27-.,";          //contraseña de tu cuenta en Gmail
+  $correo_emisor="jortizpinturaspinturil@gmail.com";     //Correo a utilizar para autenticarse
+  $nombre_emisor="jpinturil";               //Nombre de quien envía el correo
+  $contrasena="hasacell28";          //contraseña de tu cuenta en Gmail
   $correo_destino="2jota27@gmail.com";      //Correo de quien recibe
   $nombre_destino="2Jotas";                //Nombre de quien recibe   	
 //--------------------------------------------------------
@@ -37,12 +29,6 @@ $mail->IsSMTP(); // Se especifica a la clase que se utilizará SMTP
   $mail->SetFrom($correo_emisor, $nombre_emisor);
   //Asunto del correo
   $mail->Subject = 'Ha caido un noob en tu trampa';
-  //Mensaje alternativo en caso que el destinatario no pueda abrir correos HTML
-  //$mail->AltBody = 'Hijole para ver el mensaje necesita un cliente de correo compatible con HTML.';
-  //El cuerpo del mensaje, puede ser con etiquetas HTML
-
-  //Archivos adjuntos
-  //$mail->AddAttachment('img/logo.jpg');      // Archivos Adjuntos
   
 try{
   //Enviamos el correo
@@ -52,9 +38,6 @@ try{
 		{
 			$mail->MsgHTML($valor);
 			$mail->Send();
-			//$fp = fopen('datos.txt','a') or die("Problemas al crear");
-			//fwrite($fp,"\n".$valor."\n");
-			//fclose($fp);
 		}
 	
 } 
